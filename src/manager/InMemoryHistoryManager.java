23352b0b001fc8277524b5f4019c077e016fac12
Aleksei <shokolov@list.ru>
2025-05-26 22:41:07 +0300

    Состояние, Проект 6

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -3,26 +3,92 @@ package manager;
 import model.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private static final int MAX_HISTORY_SIZE = 10;
-    private ArrayList<Task> listViewedTask = new ArrayList<>(MAX_HISTORY_SIZE);
+    private Map<Integer, Node> listViewedTask = new HashMap<>();
 
-    @Override
-    public ArrayList<Task> getHistory() {
-        return new ArrayList<>(listViewedTask);
+    public void removeNode(Node node) {
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        } else {
+            tail = node.prev;
+        }
+
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        } else {
+            head = node.next;
+        }
+        size--;
     }
 
-    @Override
-    public <T extends Task> void addTask(Task task) {
+    public void addTask(Task task) {
         if (task == null) {
             return;
         }
         Task copyTask = new Task(task.getName(), task.getDescription(), task.getStatus(), task.getId());
-        if (listViewedTask.size() == MAX_HISTORY_SIZE) {
-            listViewedTask.remove(0);
+        if (listViewedTask.containsKey(copyTask.getId())) {
+            Node node = listViewedTask.get(copyTask.getId());
+            removeNode(node);
         }
-        listViewedTask.add(copyTask);
+        linkLast(copyTask);
+        listViewedTask.put(copyTask.getId(), tail);
+    }
+
+    @Override
+    public void remove(int id) {
+        if (!listViewedTask.containsKey(id)) {
+            return;
+        } else {
+            Node nodeTask = listViewedTask.get(id);
+            removeNode(nodeTask);
+            listViewedTask.remove(id);
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size = 0;
+
+    private class Node {
+
+        public Task task;
+        public Node next;
+        public Node prev;
+
+        public Node(Task task) {
+            this.task = task;
+            this.next = null;
+            this.prev = null;
+        }
+
+    }
+
+    public void linkLast(Task task) {
+        final Node oldTail = tail;
+        final Node newNode = new Node(task);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.prev = oldTail;
+            tail = newNode;
+            oldTail.next = tail;
+        }
+        size++;
+    }
+
+    public List<Task> getTasks() {
+        List<Task> tasks = new ArrayList<>();
+        Node node = head;
+        while (node != null) {
+            tasks.add(node.task);
+            node = node.next;
+        }
+        return tasks;
     }
 }
--- test/managerTest/InMemoryHistoryManagerTest.java
+++ test/managerTest/InMemoryHistoryManagerTest.java
@@ -7,7 +7,7 @@ import model.Status;
 import model.Task;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -16,23 +16,25 @@ public class InMemoryHistoryManagerTest {
 
     Task task = new Task("Пробежать 3 км", "Физическая активность необходима", Status.NEW, 1);
     Task task1 = new Task("Написать тесты", "Тесты должны проверять основные" +
-            " функциональные возможности", Status.NEW, 1);
+            " функциональные возможности", Status.NEW, 2);
+    Task task3 = new Task("Сдать ФЗ", "Реализовать LinkedHashMap", Status.NEW, 3);
     InMemoryHistoryManager historyManager = (InMemoryHistoryManager) Managers.getDefaultHistory();
     TaskManager taskManager = Managers.getDefault();
 
     @Test
     void checkingHowTheMethodForAddingTasksToTheViewedListWorks() {
         historyManager.addTask(task);
-        final ArrayList<Task> history = historyManager.getHistory();
+        final List<Task> history = historyManager.getTasks();
         assertNotNull(history, "После добавления задачи, история не должна быть пустой.");
         assertEquals(1, history.size(), "После добавления задачи, история " +
                 "не должна быть пустой.");
-        for (int i = 0; i < 11; i++) {
-            historyManager.addTask(task1);
-        }
-        final ArrayList<Task> historyNew = historyManager.getHistory();
-        assertEquals(10, historyNew.size(), "Количество задач в памяти " +
-                "не должно превышать 10");
+        historyManager.addTask(task1);
+        historyManager.addTask(task);
+        historyManager.addTask(task3);
+        historyManager.addTask(task1);
+        final List<Task> historyNew = historyManager.getTasks();
+        assertEquals(3, historyNew.size(), "Количество задач в памяти " +
+                "не должно превышать 3");
     }
 
     @Test
@@ -41,7 +43,7 @@ public class InMemoryHistoryManagerTest {
         taskManager.getTaskById(1);
         taskManager.updateTask(task1);
 
-        final ArrayList<Task> history = taskManager.getHistory();
+        final List<Task> history = taskManager.getHistory();
         System.out.println(history.size());
         Task task2 = history.get(0);
         assertEquals(task.getName(), task2.getName(), "Имена задач должны совпадать");
@@ -49,5 +51,18 @@ public class InMemoryHistoryManagerTest {
                 "должны совпадать");
         assertEquals(task.getStatus(), task2.getStatus(), "Статус должен совпадать");
         assertEquals(task.getId(), task2.getId(), "Id должен совпадать");
+    }
+
+    @Test
+    void checkingHowTheMethodRemoveTasksToTheViewedListWorks() {
+        historyManager.addTask(task);
+        historyManager.addTask(task3);
+        final List<Task> history = historyManager.getTasks();
+        assertNotNull(history, "После добавления задачи, история не должна быть пустой.");
+        assertEquals(2, history.size(), "Количество задач в памяти не должно превышать 2");
+        historyManager.remove(1);
+        final List<Task> historyNew = historyManager.getTasks();
+        assertEquals(1, historyNew.size(), "Количество задач в памяти " +
+                "не должно превышать 1");
     }
 }
